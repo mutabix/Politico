1ab8a92847fd720a1fe26aecabf3abf79afecc99
Moise1 <moiseri01@gmail.com>
2019-01-25 16:38:30 +0200

    Admin edit a political party

--- UI/javascript/admin-dashboard.js
+++ UI/javascript/admin-dashboard.js
@@ -1,17 +1,24 @@
+const dashboard = document.getElementById('dashboard-info');
 
-const dashboard = document.getElementById('dashboard-info'); 
-
-const parties = document.getElementById('parties').addEventListener('click', () =>{
+const parties = document.getElementById('parties').addEventListener('click', () => {
     dashboard.innerHTML = '<object height="600" width="1050" type="text/html" data="parties.html"></object>';
 });
 
-const createParty = document.getElementById('createParty').addEventListener('click', () =>{
+const createParty = document.getElementById('createParty').addEventListener('click', () => {
     dashboard.innerHTML = '<object height="600" width="1050" type="text/html" data="create-party.html"></object>'
-}); 
+});
 
 
-const createOffice = document.getElementById('createOffice').addEventListener('click', () =>{
+const createOffice = document.getElementById('createOffice').addEventListener('click', () => {
     dashboard.innerHTML = '<object height="500" width="1050" type="text/html" data="create-office.html"></object>'
-}); 
+});
 
 
+
+const editParty = document.getElementById('editParty').addEventListener('click', () => {
+    dashboard.innerHTML = '<object height="600" width="1050" type="text/html" data="edit-party.html"></object>'
+});
+
+let editor = document.getElementsByClassName('table-data'); 
+editor.isContentEditable; 
+editor.contentEditable = true;
