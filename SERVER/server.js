2231c05488985408139a452838ec3709cba714bd
Moise1 <moiseri01@gmail.com>
2019-01-29 17:14:33 +0200

    Create party API endpoint

---
+++ SERVER/server.js
@@ -0,0 +1,21 @@
+
+import express from 'express'; 
+import partyRouter from './routes/parties';
+
+const app = express(); 
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json()); 
+app.use(partyRouter);
+
+app.get('/', (req, res) =>{
+    res.status(200).json({
+        message: 'Welcome to Politico'
+    });
+});
+
+const port = app.listen(process.env.PORT || 3000, () =>{
+ console.log('App listening on port 3000 ' );
+});
+
+export default app;
