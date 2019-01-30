0f8261ad374fb3c0638b290469c3310a31c12b17
Moise1 <moiseri01@gmail.com>
2019-01-30 14:12:33 +0200

    User get one gov office

--- SERVER/controllers/offices.js
+++ SERVER/controllers/offices.js
@@ -8,6 +8,16 @@ class Office{
     static getAllOffices(req, res){
         return res.send(offices);
     }
+
+    static getOneOffice(req, res){
+        const office = offices.find(of => of.id === parseInt(req.params.id)); 
+        if(!office){
+            return res.status(404).send('Office not found');
+        }
+
+        res.send(office);
+    }
+
 }
 
 
