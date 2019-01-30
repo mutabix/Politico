80368bf07e9996ab54e868e613689ee98974c7b1
Moise1 <moiseri01@gmail.com>
2019-01-30 15:37:30 +0200

    Delete function updated

--- SERVER/controllers/parties.js
+++ SERVER/controllers/parties.js
@@ -2,60 +2,14 @@ import Joi from 'joi';
 import parties from '../models/parties';
 class Party {
 
-    // Create a party
-    static createParty(req, res) {
-
-        const party = {
-            id: parties.length + 1,
-            name: req.body.name,
-            hqAddress: req.body.hqAddress,
-            logoUrl: req.body.logoUrl
-        };
-        parties.push(party);
-        res.send(parties);
-    }
-
-    // Get all Parties 
-
-    static getAllParties(req, res) {
-        res.send(parties);
-    }
-
-
-    // Get one party
-    static getOneParty(req, res) {
-        const party = parties.find(p => p.id === parseInt(req.params.id));
-        if (!party) {
-            return res.status(404).send('Party not found');
-        }
-
-        res.send(party);
-    }
-
-
-    static updateParty(req, res) {
-
-        const party = parties.find(p => p.id === parseInt(req.params.id));
-        if (!party) {
-            return res.status(400).send('Party not found');
-        }
-
-        //Update Party
-
-        party.name = req.body.name;
-        party.hqAddress = req.body.hqAddress;
-        party.logoUrl = req.body.logoUrl;
-
-        res.send(party);
-    }
-
-
-    // Delete Party
-
+   
     static deleteParty(req, res) {
         const party = parties.find(p => p.id === parseInt(req.params.id));
         if (!party) {
-            return res.status(400).send('Party not found');
+            return res.send({
+                status: 404, 
+                error: `Party with ID ${req.params.id} is not found!`
+            });
         };
     
         const index = parties.indexOf(party);
