d6a0575f850515548bcc3deca6fb8479e30f6c14
Moise1 <moiseri01@gmail.com>
2019-02-13 19:40:56 +0200

    update admin-dashboard

--- UI/javascript/adminLogin.js
+++ UI/javascript/adminLogin.js
@@ -2,7 +2,7 @@ function formValidator() {
     const result = document.getElementById("wrongInput");
     const pwd = document.getElementById("password").value;
     const name = document.getElementById('name').value;
-    if (pwd !== '999999' && name !== 'moise') {
+    if (pwd !== 999999 && name !== 'moise') {
         result.innerHTML = 'Password and Username didn\'t match!'
         return;
     };
